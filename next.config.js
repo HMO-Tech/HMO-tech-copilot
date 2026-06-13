1b5afa7b44def98d159bcd5d585e985895610dd4
H. Mohseni <279184999+HMO-Tech@users.noreply.github.com>
2026-06-13 18:25:26 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // آپشن‌های اضافی و نامعتبر که باعث کرش می‌شدند کاملاً حذف شدند
 };
 
 module.exports = nextConfig;
