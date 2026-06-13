da970c5abf821980a023ca3ca7cb280d7f4a6cf1
H. Mohseni <279184999+HMO-Tech@users.noreply.github.com>
2026-06-13 19:41:46 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 🎯 این بخش مسیر اصلی سایت را به صورت اجباری مپ می‌کند تا ارور ۴۰۴ دامین برطرف شود
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
