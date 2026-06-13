444bd0f2181c91750dfe72180ea4b06306945645
H. Mohseni <279184999+HMO-Tech@users.noreply.github.com>
2026-06-13 18:27:36 +0330

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="fa" dir="rtl">
-        <Head>
-          {/* کدهای متا یا لینک فونت‌ها اگر داری اینجا قرار می‌گیرند */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
